6a601b21dcd784d037c250916cb4a8ad17f22823
MehdiLam <52042@etu.he2b.be>
2023-08-04 13:35:16 +0200

    creating minmax base

---
+++ minmax.py
@@ -0,0 +1,13 @@
+class Node():
+    def __init__(self, value):
+        # Heurestic value
+        self.value = value
+        self.child_nodes=[]
+        self.child_count=0
+    def AddNode(self,node):
+        
+        self.child_nodes.append(node)
+        self.child_count=len(self.child_nodes)
+def minmax(depth):
+    # Choix de la profondeur à 2 pour l'instant
+    pass
